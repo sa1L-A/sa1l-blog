875f02198430bed0660da7b2bad350823692ed11
sa1L <send2salam@163.com>
2023-09-04 23:07:29 +0800

    更新shell

--- update_blog.sh
+++ update_blog.sh
@@ -1,9 +1,12 @@
-## env path start
-PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin:/usr/local/go/bin"
+## env definit start
+PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin:/usr/local/go/bin:/opt/homebrew/bin"
+SHELL_FOLDER=$(dirname "$0")
 ## script start
+cd $SHELL_FOLDER
 echo "1.start to pull blog."
 git pull 
 echo "2.start to pull theme"
-test -e ./themes/paper-mod  && git pull  || git clone https://github.com/sa1L-A/paper-mod.git --depth=1 /themes/paper-mod
+test -e ./themes/paper-mod  && (cd ./themes/paper-mod && git pull)  || git clone https://github.com/sa1L-A/paper-mod.git --depth=1 themes/paper-mod
 echo "3.reload"
+cd $SHELL_FOLDER
 hugo
